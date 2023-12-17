8ed86caaf1f2ed0fea0fd7c9f5121bbee605b65b
Piotr Bielak <piotrbielak2@gmail.com>
2023-12-17 16:35:45 +0100

    Day 16 - part two

--- day16/main.py
+++ day16/main.py
@@ -2,14 +2,9 @@
 from typing import NamedTuple
 
 
-class Point2D(NamedTuple):
-    x: int
-    y: int
-
-
 class Beam(NamedTuple):
-    position: Point2D
-    direction: Point2D
+    position: tuple[int, int]
+    direction: tuple[int, int]
 
 
 InputData = list[list[str]]
@@ -21,18 +16,57 @@ def parse_input(path: str) -> InputData:
 
 
 def solve_part_one(data: InputData) -> int:
-    energies = trace_beam(grid=data)
+    energies = trace_beam(
+        grid=data,
+        start_beam=Beam(position=(0, 0), direction=(1, 0))
+    )
     answer = sum([sum(1 if c > 0 else 0 for c in row) for row in energies])
 
     return answer
 
 
-def trace_beam(grid: InputData):
+def solve_part_two(data: InputData) -> int:
+    start_beams = [
+        # Top
+        *[
+            Beam(position=(x, 0), direction=(0, 1))
+            for x in range(len(data[0]))
+        ],
+        # Bottom
+        *[
+            Beam(position=(x, len(data) - 1), direction=(0, -1))
+            for x in range(len(data[0]))
+        ],
+        # Left
+        *[
+            Beam(position=(0, y), direction=(1, 0))
+            for y in range(len(data))
+        ],
+        # Right
+        *[
+            Beam(position=(len(data[0]) - 1, y), direction=(-1, 0))
+            for y in range(len(data))
+        ],
+    ]
+
+    answer = 0
+
+    for start_beam in start_beams:
+        energies = trace_beam(grid=data, start_beam=start_beam)
+        n_energized_tiles = sum([
+            sum(1 if c > 0 else 0 for c in row)
+            for row in energies
+        ])
+
+        answer = max(answer, n_energized_tiles)
+
+    return answer
+
+
+def trace_beam(grid: InputData, start_beam: Beam) -> list[list[int]]:
     energies = [[0] * len(grid[0]) for _ in range(len(grid))]
 
-    beams = [
-        Beam(position=Point2D(0, 0), direction=Point2D(1, 0)),
-    ]
+    beams = [start_beam]
     traced = set()
 
     while beams:
@@ -40,9 +74,6 @@ def trace_beam(grid: InputData):
 
         if x < 0 or x >= len(grid[0]) or y < 0 or y >= len(grid):
             continue
-
-        #print(f"Current beam: ({x}, {y}) D=({dx}, {dy})")
-        #breakpoint()
 
         if (x, y, dx, dy) in traced:
             continue
@@ -77,27 +108,20 @@ def trace_beam(grid: InputData):
                     dx, dy = 1, 0
         elif grid[y][x] == "|":
             if dx == 1 or dx == -1:
-                beams.append(Beam(position=Point2D(x, y - 1), direction=Point2D(0, -1)))
-                beams.append(Beam(position=Point2D(x, y + 1), direction=Point2D(0, 1)))
+                beams.append(Beam(position=(x, y - 1), direction=(0, -1)))
+                beams.append(Beam(position=(x, y + 1), direction=(0, 1)))
                 continue
         elif grid[y][x] == "-":
             if dy == 1 or dy == -1:
-                beams.append(Beam(position=Point2D(x - 1, y), direction=Point2D(-1, 0)))
-                beams.append(Beam(position=Point2D(x + 1, y), direction=Point2D(1, 0)))
+                beams.append(Beam(position=(x - 1, y), direction=(-1, 0)))
+                beams.append(Beam(position=(x + 1, y), direction=(1, 0)))
                 continue
         else:
             raise ValueError(f"Unknown tile: '{grid[y][x]}'")
 
-        beams.append(Beam(position=Point2D(x + dx, y + dy), direction=Point2D(dx, dy)))
+        beams.append(Beam(position=(x + dx, y + dy), direction=(dx, dy)))
 
     return energies
-    #print('\n'.join(''.join(['.' if c == 0 else "#" for c in row]) for row in energies))
-
-
-def solve_part_two(data: InputData) -> int:
-    answer = ...
-
-    return answer
 
 
 def run_tests():
@@ -109,7 +133,7 @@ def run_tests():
 
     part_two = solve_part_two(data)
     print("Example - part 2:", part_two)
-    assert part_two == ...
+    assert part_two == 51
 
 
 def main():
